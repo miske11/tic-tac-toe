c29446e4a5b653094ed7e9032995e4de1f539f62
Luka Misic <lukamisic2001@gmail.com>
2024-12-08 18:30:28 +0100

    Added score tracking for players

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Gameboard = (function () {
+function Gameboard() {
   const grid = [];
   const dimension = 3;
 
@@ -30,7 +30,7 @@ const Gameboard = (function () {
     playMove,
     printGameboard,
   };
-})();
+}
 
 function Cell() {
   let value = 0;
@@ -55,14 +55,17 @@ function GameController(
     {
       name: playerOneName,
       token: 1,
+      score: 0,
     },
     {
       name: playerTwoName,
       token: 2,
+      score: 0,
     },
   ];
   const dimension = 3;
   let activePlayer = players[0];
+  let board = Gameboard();
 
   const switchActivePlayer = () => {
     if (activePlayer === players[0]) activePlayer = players[1];
@@ -72,28 +75,34 @@ function GameController(
   const getActivePlayer = () => activePlayer;
 
   const printNewRound = () => {
-    Gameboard.printGameboard();
+    board.printGameboard();
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
   const playRound = (row, column) => {
     console.log(`${activePlayer.name} marks the [${row}, ${column}]`);
-    const played = Gameboard.playMove(activePlayer.token, row, column);
-    if (played) {
-      let over = checkForWinner();
-      if (!over) {
-        const tie = checkForTie();
-        if (!tie) {
-          switchActivePlayer();
-          printNewRound();
-        } else {
-          console.log(`It's a tie.`);
-        }
+    board.playMove(activePlayer.token, row, column);
+    let over = checkForWinner();
+    if (!over) {
+      const tie = checkForTie();
+      if (!tie) {
+        switchActivePlayer();
+        printNewRound();
+        return tie;
       } else {
-        console.log(`${activePlayer.name} won!!!`);
+        console.log(`It's a tie.`);
+        return tie;
       }
+    } else {
+      console.log(`${activePlayer.name} won!!!`);
+      activePlayer.score++;
+      return over;
     }
   };
+
+  const getBoard = () => board;
+
+  const isOver = () => over;
 
   const checkForWinner = () => {
     let xcounter = 0;
@@ -101,7 +110,7 @@ function GameController(
     let zcounter = 0;
     for (let i = 0; i < dimension; i++) {
       for (let j = 0; j < dimension; j++) {
-        if (Gameboard.getGrid()[i][j].getValue() == activePlayer.token) {
+        if (board.getGrid()[i][j].getValue() == activePlayer.token) {
           ycounter++;
           if (ycounter == 3) return true;
         }
@@ -110,7 +119,7 @@ function GameController(
     }
     for (let i = 0; i < dimension; i++) {
       for (let j = 0; j < dimension; j++) {
-        if (Gameboard.getGrid()[j][i].getValue() == activePlayer.token) {
+        if (board.getGrid()[j][i].getValue() == activePlayer.token) {
           xcounter++;
           if (xcounter == 3) return true;
         }
@@ -118,15 +127,13 @@ function GameController(
       xcounter = 0;
     }
     for (let i = 0; i < dimension; i++) {
-      if (Gameboard.getGrid()[i][i].getValue() == activePlayer.token)
-        zcounter++;
+      if (board.getGrid()[i][i].getValue() == activePlayer.token) zcounter++;
     }
     if (zcounter == 3) return true;
     zcounter = 0;
     for (let i = 0; i < dimension; i++) {
       if (
-        Gameboard.getGrid()[dimension - i - 1][i].getValue() ==
-        activePlayer.token
+        board.getGrid()[dimension - i - 1][i].getValue() == activePlayer.token
       )
         zcounter++;
     }
@@ -138,7 +145,7 @@ function GameController(
     let tie = true;
     for (let i = 0; i < dimension; i++) {
       for (let j = 0; j < dimension; j++) {
-        if (Gameboard.getGrid()[i][j].getValue() == 0) {
+        if (board.getGrid()[i][j].getValue() == 0) {
           tie = false;
         }
       }
@@ -151,6 +158,8 @@ function GameController(
   return {
     playRound,
     getActivePlayer,
+    getBoard,
+    isOver,
   };
 }
 
@@ -158,6 +167,7 @@ function ScreenControler() {
   const game = GameController();
   const grid = document.querySelector(".grid");
   const turn = document.querySelector(".turn");
+  const board = game.getBoard();
 
   const getSign = (value) => {
     switch (value) {
@@ -170,13 +180,35 @@ function ScreenControler() {
     }
   };
 
+  const disableScreen = () => {
+    grid.textContent = "";
+    const activePlayer = game.getActivePlayer();
+    const nextRoundBtn = document.querySelector('.new');
+
+    turn.textContent = `${activePlayer.name} won!!!`;
+    updatePlayerDisplay(activePlayer);
+
+    board.getGrid().forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("field");
+        cellButton.dataset.column = colIndex;
+        cellButton.dataset.row = rowIndex;
+        cellButton.textContent = getSign(cell.getValue());
+        grid.appendChild(cellButton);
+        cellButton.disabled = true;
+        nextRoundBtn.style.visibility = "visible";
+      });
+    });
+  };
+
   const updateScreen = () => {
     grid.textContent = "";
     const activePlayer = game.getActivePlayer();
 
     turn.textContent = `${activePlayer.name}'s turn...`;
 
-    Gameboard.getGrid().forEach((row, rowIndex) => {
+    board.getGrid().forEach((row, rowIndex) => {
       row.forEach((cell, colIndex) => {
         const cellButton = document.createElement("button");
         cellButton.classList.add("field");
@@ -194,8 +226,21 @@ function ScreenControler() {
 
     if (!selectedColumn || !selectedRow) return;
 
-    game.playRound(selectedRow, selectedColumn);
-    updateScreen();
+    const over = game.playRound(selectedRow, selectedColumn);
+    if (!over)
+      updateScreen();
+    else 
+      disableScreen();
+  }
+
+  const updatePlayerDisplay = (player) => {
+    const scoreDiv = document.createElement('p');
+    scoreDiv.classList.add('container');
+    scoreDiv.textContent = player.score;
+    const playerDiv = document.getElementById(`c${player.token}`)
+    const previusScore = document.querySelector(`#c${player.token} > .score` );
+    playerDiv.removeChild(previusScore);
+    playerDiv.appendChild(scoreDiv);
   }
 
   grid.addEventListener("click", clickHandlerGrid);
